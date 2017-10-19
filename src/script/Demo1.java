04906ec045f37e89d3657851e72684b09b92254e
ROHITH KUMAR D <32764045+Rohithkumar76@users.noreply.github.com>
2017-10-19 16:10:40 +0530

    Initial commit

---
+++ src/script/Demo1.java
@@ -0,0 +1,13 @@
+package script;
+
+import generic.Lib;
+
+public class Demo1 {
+	
+
+
+	public static void main(String[] args) {
+		System.out.println(Lib.getPpt("./config.properties","OBJ"));
+	}
+
+}
